13edb897c72156d9a495aa4fd3e213db987b5cfc
PottierLoic <loic.pottier@live.fr>
2023-03-05 21:56:34 +0100

    correcting a mistake

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Floowo
-Fluid simulation written in python
+Fluid simulation
--- cell.py
+++
@@ -1,9 +0,0 @@
-"""
-Cell class.
-    Author: Loïc Pottier.
-    Creation date: 27/02/2023.
-"""
-
-class Cell:
-    def __init__(self) -> None:
-        pass
